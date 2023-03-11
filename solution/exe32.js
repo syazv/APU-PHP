bcecded86517c3fb81a29af4dfd6cd828e17a085
syazv <syazanifaudzi@gmail.com>
2023-03-11 17:07:04 +0800

    update

--- solution/exe32.js
+++ solution/exe32.js
@@ -1,7 +1,11 @@
-function getAge(YOB) {
-  const d = new Date();
-  let currentYear = d.getFullYear();
-  let age = currentYear - Number(YOB);
-  console.log(age);
-  return age;
+// function getAge(YOB) {
+//   const d = new Date();
+//   let currentYear = d.getFullYear();
+//   let age = currentYear - Number(YOB);
+//   console.log(age);
+//   return age;
+// }
+
+var getAge = function (YOB) {
+  return new Date().getFullYear - YOB;
 }
